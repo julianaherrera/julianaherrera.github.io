b6189f24247cf12e3d448ea8b8e846feba2aa281
julianaherrera <julihc10@gmail.com>
2016-02-11 10:45:37 -0800

    data of mean prices per year

--- script prices.R
+++ script prices.R
@@ -18,22 +18,32 @@ library(tidyr)
 
 data <- read_excel('prices.xlsx') 
 colnames(data) <- tolower(colnames(data))
-CPI <- read_excel('CPI.xlsx') # Consumer Price Index - source: World Bank
-Biomass <- read_csv()
-  
+CPI <- read_excel('CPI2.xlsx') # Consumer Price Index - source: World Bank
+CPIm <- read_excel('CPI.xlsx') # Consumer Price Index by month : World Bank
+# Biomass <- read_csv()
+ 
 yrprices <- (data) %>%
-  select(-month, -spp) %>%
+  select(-fishery, -spp) %>%
   group_by(year) %>% 
   summarize(meanprice = mean(price, na.rm=T)) %>% 
   bind_cols(CPI) %>% 
   mutate(corrected = meanprice/cpi*cpi[24])
-head(yrprices) # corrected prices for inflation = corrected
+head(yrprices) # corrected prices for inflation. Baseline year = 2013
 
 p <-ggplot(yrprices) +
   geom_line(aes(x=year, y=meanprice, colour = 'meanprice')) +
   geom_line(aes(x=year, y=corrected, colour ='corrected')) +
   theme_bw()
 p
+
+monthprices <- (data) %>%
+  select(-fishery, -spp) %>%
+  group_by(month, year) %>% 
+  summarize(meanprice = mean(price, na.rm=T)) %>% 
+  bind_cols(CPI) %>% 
+  mutate(corrected = meanprice/cpi*cpi[289])
+head(monthprices) # corrected prices for inflation. Baseline year = 2013
+
 
 #==PART ===========================================================
 
